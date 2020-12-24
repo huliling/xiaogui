2d09e715d031f8d38ba58ee191623eab76ea6afa
hll2550467534@outlook.com <hll2550467534@outlook.com>
2020-12-24 09:17:43 +0800

    添加项目文件。

---
+++ HLL/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL;
+using Models;
+namespace HLL.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAll(int pageIndex=1,int pageSize=10)
+        {
+            return Json(StudentBLL.QueryAll(pageIndex,pageSize),JsonRequestBehavior.AllowGet);
+        }
+    }
+}
